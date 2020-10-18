dedffcba8f4d7094e1d632ee45b712161fd9a64a
Rasyid Ridlo <61292802+RsydRidloo@users.noreply.github.com>
2020-10-18 11:50:43 +0700

    Update main.js

--- main.js
+++ main.js
@@ -2,6 +2,27 @@ function main() {
     var canvas = document.getElementById('myCanvas');
     var gl = canvas.getContext('webgl');
   
+      // Definisi titik-titik pembentuk segitiga
+  /**
+   * A = (-0.5, 0.5)
+   * B = (0.5, 0.5)
+   * C = (0.5, -0.5)
+   * D = (-0.5, -0.5)
+   */
+  var vertices = [
+    -0.5, 0.5, 1.0, 0.0, 0.0,      // Titik A
+    0.5, 0.5, 0.0, 1.0, 0.0,       // Titik B
+    0.5, -0.5, 0.0, 0.0, 1.0,      // Titik C
+    -0.5, 0.5, 1.0, 0.0, 0.0,      // Titik A
+    0.5, -0.5, 0.0, 0.0, 1.0,      // Titik C
+    -0.5, -0.5, 0.0, 1.0, 0.0      // Titik D
+  ];
+
+  var vertexBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+  gl.bindBuffer(gl.ARRAY_BUFFER, null);
+
     // Ibaratnya di bawah ini adalah .c
     var vertexShaderSource = `
       void main() {
@@ -41,13 +62,34 @@ function main() {
     gl.useProgram(shaderProgram);
   
     gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
-    var aPosition = gl.getAttribLocation(shaderProgram, "a_Position");
-    gl.vertexAttribPointer(aPosition, 2, gl.FLOAT, false, 0, 0);
-    gl.enableVertexAttribArray(aPosition);
+  var aPosition = gl.getAttribLocation(shaderProgram, "a_Position");
+  var aColor = gl.getAttribLocation(shaderProgram, "a_Color");
+  gl.vertexAttribPointer(
+    aPosition, 
+    2, 
+    gl.FLOAT, 
+    false, 
+    5 * Float32Array.BYTES_PER_ELEMENT, 
+    0);
+  gl.vertexAttribPointer(
+    aColor, 
+    3, 
+    gl.FLOAT, 
+    false, 
+    5 * Float32Array.BYTES_PER_ELEMENT, 
+    2 * Float32Array.BYTES_PER_ELEMENT);
+  gl.enableVertexAttribArray(aPosition);
+  gl.enableVertexAttribArray(aColor);
 
-    var primitive = gl.POINTS;
-    var offset = 0;
-    var nVertex = 3;
-    gl.drawArrays(primitive, offset, nVertex);
-  }
+  gl.clearColor(0.0, 0.22, 0.5, 1.0);
+  gl.clear(gl.COLOR_BUFFER_BIT);
+  gl.viewport(100, 0, canvas.height, canvas.height);
+
+  var primitive = gl.TRIANGLES;
+  var offset = 0;
+  var nVertex = 6;
+  gl.drawArrays(primitive, offset, nVertex);
+}
+
+
   
